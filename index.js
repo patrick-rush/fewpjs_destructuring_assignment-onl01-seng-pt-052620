f89ceea1e1f8fdaaf2e9dd8eae7d604c8ec0f45a
patrick-rush <patrick.a.rush@gmail.com>
2020-12-15 21:52:30 -0500

    commit tree 7ae2f3bb9ff999164e8cd69d7fe2ae320eb3af45

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const farmAnimals = 'cow horse sheep pig chicken'
 
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ')
+
+const [Bessie, , Dolly, Babe, Little] = farmAnimals.split(' ')
+
+const [black_and_white, , black, pink] = farmAnimals.split(' ')
+
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+const [red, orange, yellow, green, blue, indigo, violet] = colors
+
+const [r, o, y, g, b, i, v] = colors
+
+const [, , , , , indg] = colors
 
 const muppet = {
   name: 'Kermit',
@@ -9,6 +21,8 @@ const muppet = {
   job: 'Host of The Muppet Show',
   partner: 'Miss Piggy'
 };
+
+const { name, color, song, job, partner } = muppet;
 
 const k_muppet = {
   k_name: 'Kermit',
@@ -24,6 +38,9 @@ const k_muppet = {
   k_job: 'Host of The Muppet Show',
   k_partner: 'Miss Piggy'
 };
+
+const { k_color, k_job, k_partner } = k_muppet;
+const { song_2, song_4 } = k_muppet.k_album.the_muppet_movie
 
 // Strings
 
